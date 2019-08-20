9dec25196c52ac20eb46e8cbb0fb020b259ecd9a
sallysy8 <sallysy8@hotmail.com>
2019-08-20 14:41:27 -0700

    Updated the code with serviceand tvshow-search component and use angular material to make the webpage more beautiful

--- src/app/current-show/current-show.component.ts
+++ src/app/current-show/current-show.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentShow } from '../icurrent-show';
+import { ShowService } from '../show.service';
 
 @Component({
   selector: 'app-current-show',
   templateUrl: './current-show.component.html',
   styleUrls: ['./current-show.component.css']
 })
+
 export class CurrentShowComponent implements OnInit {
 
-  constructor() { }
+  current: ICurrentShow
+  constructor(private ShowService: ShowService) {
+  }
 
   ngOnInit() {
+    this.ShowService.getCurrentTVshow('The Boys').subscribe(data => this.current = data);
   }
 
 }
+
---
+++ src/app/icurrent-show.ts
@@ -0,0 +1,10 @@
+import { Time } from '@angular/common';
+
+export interface ICurrentShow {
+    network: string
+    webchannel: string
+    showname: string
+    airdate: Date
+    showimage: string
+    showdescription: string
+}
