55d50fe4913090ca693fd20bc685338a4f978b95
sallysy8 <sallysy8@hotmail.com>
2019-10-02 15:26:58 -0700

    updates to deploy to heroku

--- src/app/icurrent-show.ts
+++ src/app/icurrent-show.ts
@@ -1,11 +1,11 @@
 import { Time } from "@angular/common";
 
 export interface ICurrentShow {
-  channel: number;
-  showname: string;
-  airdate: string;
-  time: string;
-  days: [string];
-  showimage: string;
-  showdescription: string;
+  channel: number,
+  showname: string,
+  airdate: string,
+  time: string,
+  days: [string],
+  showimage: string,
+  showdescription: string
 }
